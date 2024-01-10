01329fb765939247161134a2ba396e08232b2997
Yurii <152527309+Waitin4Silksong@users.noreply.github.com>
2024-01-10 17:38:41 +0300

    Add files via upload

---
+++ Dzz1.cs
@@ -0,0 +1,78 @@
+﻿
+int size = 5;
+int[] A = new int[size];
+for (int i = 0; i < size; i++)
+{
+    A[i] = Convert.ToInt32(Console.ReadLine());
+}
+foreach (var num in A)
+{
+    Console.Write($"{num} ");
+}
+int min = A[0];
+int max = A[0];
+int sum = 0;
+int dob = 1;
+int sumeven = 0;
+foreach (var num in A)
+{
+    if (num < min)
+    {
+        min = num;
+    }
+    if (num > max)
+    {
+        max = num;
+    }
+    sum += num;
+    dob *= num;
+    if (num % 2 ==0)
+    {
+        sumeven += num;
+    }
+}
+///////////////////////////////////////////
+int row = 3;
+int col = 4;
+int[,] B = new int[row, col];
+Random random = new Random();
+for (int i = 0; i < row; i++)
+{
+    for (int j = 0; j < col; j++)
+    {
+        B[i, j] = random.Next(1, 100);
+    }
+}
+for (int i = 0; i < row; i++)
+{
+    for (int j = 0; j < col; j++)
+    {
+        Console.Write($"{B[i, j]} ");
+    }
+    Console.WriteLine();
+}
+int min1 = B[0, 0];
+int max1 = B[0, 0];
+int sum1 = 0;
+int dob1 = 1;
+int sumodd = 0;
+foreach (var num in B)
+{
+    if (num < min1)
+    {
+        min1 = num;
+    }
+    if (num > max1)
+    {
+        max1 = num;
+    }
+    sum1 += num;
+    dob1 *= num;
+    if (num % 2 != 0)
+    {
+        sumodd += num;
+    }
+}
+
+
+
---
+++ Dzz2.cs
@@ -0,0 +1,46 @@
+﻿int[,] arr = new int[5, 5];
+Random rand = new Random();
+for (int i = 0; i < 5; i++)
+{
+	for (int j = 0; j < 5; j++)
+	{
+		arr[i, j] = rand.Next(-100, 101);
+	}
+}
+for (int i = 0; i < 5; i++)
+{
+	for (int j = 0; j < 5; j++)
+	{
+		Console.Write(arr[i, j]);
+	}
+	Console.WriteLine();
+}
+int min = arr[0, 0];
+int max = arr[0, 0];
+int sum = 0;
+bool numsMinMax = false;
+foreach (var num in arr)
+{
+    if (num < min)
+    {
+        min = num;
+    }
+    if (num > max)
+    {
+        max = num;
+    }
+}
+for (int i = 0; i < 5; i++)
+{
+	for (int j = 0; j < 5; j++)
+	{
+		if (arr[i, j] == min || arr[i, j] == max)
+		{
+			numsMinMax = !numsMinMax;
+		}
+		if (numsMinMax == true)
+		{
+			sum += arr[i, j];
+		}
+	}
+}
